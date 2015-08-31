24bedbe9c1af0ddfd1044321dbd83ec0b107ae22
Lásaro <ladumer@hotmail.com>
2015-08-31 15:04:07 -0300

    Bootstrap Rise
    Initial bootstrap implementation

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,65 +1,23 @@
 ( function( $ ) {
 $( document ).ready(function() {
-$('#cssmenu li.has-sub>a').on('click', function(){
-		$(this).removeAttr('href');
-		var element = $(this).parent('li');
-		if (element.hasClass('open')) {
-			element.removeClass('open');
-			element.find('li').removeClass('open');
-			element.find('ul').slideUp();
+
+	$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+		// Avoid following the href location when clicking
+		event.preventDefault();
+		// Avoid having the menu to close when clicking
+		event.stopPropagation();
+		// If a menu is already open we close it
+		$('ul.dropdown-menu [data-toggle=dropdown]').parent().removeClass('open');
+		// opening the one you clicked on
+		$(this).parent().addClass('open');
+		var menu = $(this).parent().find("ul");
+		var menupos = menu.offset();
+		if ((menupos.left + menu.width()) + 30 > $(window).width()) {
+			var newpos = - menu.width();
+		} else {
+			var newpos = $(this).parent().width();
 		}
-		else {
-			element.addClass('open');
-			element.children('ul').slideDown();
-			element.siblings('li').children('ul').slideUp();
-			element.siblings('li').removeClass('open');
-			element.siblings('li').find('li').removeClass('open');
-			element.siblings('li').find('ul').slideUp();
-		}
+		menu.css({ left:newpos });
 	});
-
-	$('#cssmenu>ul>li.has-sub>a').append('<span class="holder"></span>');
-
-	(function getColor() {
-		var r, g, b;
-		var textColor = $('#cssmenu').css('color');
-		textColor = textColor.slice(4);
-		r = textColor.slice(0, textColor.indexOf(','));
-		textColor = textColor.slice(textColor.indexOf(' ') + 1);
-		g = textColor.slice(0, textColor.indexOf(','));
-		textColor = textColor.slice(textColor.indexOf(' ') + 1);
-		b = textColor.slice(0, textColor.indexOf(')'));
-		var l = rgbToHsl(r, g, b);
-		if (l > 0.7) {
-			$('#cssmenu>ul>li>a').css('text-shadow', '0 1px 1px rgba(0, 0, 0, .35)');
-			$('#cssmenu>ul>li>a>span').css('border-color', 'rgba(0, 0, 0, .35)');
-		}
-		else
-		{
-			$('#cssmenu>ul>li>a').css('text-shadow', '0 1px 0 rgba(255, 255, 255, .35)');
-			$('#cssmenu>ul>li>a>span').css('border-color', 'rgba(255, 255, 255, .35)');
-		}
-	})();
-
-	function rgbToHsl(r, g, b) {
-	    r /= 255, g /= 255, b /= 255;
-	    var max = Math.max(r, g, b), min = Math.min(r, g, b);
-	    var h, s, l = (max + min) / 2;
-
-	    if(max == min){
-	        h = s = 0;
-	    }
-	    else {
-	        var d = max - min;
-	        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-	        switch(max){
-	            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-	            case g: h = (b - r) / d + 2; break;
-	            case b: h = (r - g) / d + 4; break;
-	        }
-	        h /= 6;
-	    }
-	    return l;
-	}
 });
 } )( jQuery );
